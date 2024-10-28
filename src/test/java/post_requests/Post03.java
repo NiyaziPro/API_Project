4e59c61c1d84b9dea310d861286aba442b657984
unknown <nyzjunior@gmail.com>
2024-10-28 11:02:22 +0100

    11:02 Monday 28.10.2024

--- src/test/java/post_requests/Post03.java
+++ src/test/java/post_requests/Post03.java
@@ -9,7 +9,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static io.restassured.RestAssured.*;
-import static org.testng.Assert.assertEquals;
+import static org.testng.AssertJUnit.assertEquals;
 
 
 public class Post03 extends JsonPlaceHolderBaseUrl {
@@ -38,16 +38,16 @@ Given
  */
 
     @Test
-    public void test01() {
+    public void testSerialization() {
 
         //Set the url :
-        specification.pathParam("first","todos");
+        specification.pathParam("first", "todos");
 
         // Set the expected data / payload :
-        Map<String,Object> payload = new HashMap<>();
-        payload.put("userId",55);
-        payload.put("title","Tidy your room");
-        payload.put("completed",false);
+        Map<String, Object> payload = new HashMap<>();
+        payload.put("userId", 55);
+        payload.put("title", "Tidy your room");
+        payload.put("completed", false);
 
 
         // Send the request and get the response :
@@ -63,12 +63,54 @@ pom.xml dosyamiza eklemis oldugumuz Jackson databind .. gibi kütüphaneler seri
 de serialization islemlerini otomatik olarak yapar, bizim extra bir sey yapmamiza gerek kalmaz
  */
 
+        payload.put("id", 201);
 
         // Do assertions
 
-        assertEquals(jsonPath.getInt("userId"),payload.get("userId"));
-        assertEquals(jsonPath.getString("title"),payload.get("title"));
-        assertEquals(jsonPath.getBoolean("completed"),payload.get("completed"));
+        assertEquals(201, response.statusCode());
+        assertEquals(payload.get("userId"), jsonPath.getInt("userId"));
+        assertEquals(payload.get("title"), jsonPath.getString("title"));
+        assertEquals(payload.get("completed"), jsonPath.getBoolean("completed"));
+        assertEquals(payload.get("id"), jsonPath.getInt("id"));
+
+
+    }
+
+    @Test
+    public void testDeSerialization() {
+
+        //Set the url :
+        specification.pathParam("first", "todos");
+
+        // Set the expected data / payload :
+        Map<String, Object> payload = new HashMap<>();
+        payload.put("userId", 55);
+        payload.put("title", "Tidy your room");
+        payload.put("completed", false);
+
+
+        // Send the request and get the response :
+
+        Response response = given(specification).body(payload).when().post("{first}");
+        response.prettyPrint();
+
+        JsonPath jsonPath = response.jsonPath();
+
+
+        payload.put("id", 201);
+
+        // Do assertions
+        Map<String, Object> actualData = response.as(HashMap.class);
+        System.out.println("Actual data : " + actualData);
+        System.out.println("Expected data :" + payload);
+
+        assertEquals(payload, actualData);
+
+        assertEquals(payload.get("userId"),actualData.get("userId"));
+        assertEquals(payload.get("title"),actualData.get("title"));
+        assertEquals(payload.get("completed"),actualData.get("completed"));
+        assertEquals(payload.get("id"),actualData.get("id"));
+
 
     }
 }
