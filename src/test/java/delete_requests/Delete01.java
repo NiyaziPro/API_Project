a39dfb527230ba70b8b0e77d8cf82ee93d4a3c76
unknown <nyzjunior@gmail.com>
2024-10-28 11:49:09 +0100

    11:49 Monday 28.10.2024

---
+++ src/test/java/delete_requests/Delete01.java
@@ -0,0 +1,49 @@
+package delete_requests;
+
+import baseUrl.JsonPlaceHolderBaseUrl;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+import static org.testng.AssertJUnit.assertEquals;
+
+public class Delete01 extends JsonPlaceHolderBaseUrl {
+
+    /*
+    Given
+        https://jsonplaceholder.typicode.com/todos/198
+    When
+        I send DELETE Request to the Url
+    Then
+        Status code is 200
+    And Response body is { }
+*/
+
+    @Test
+    public void test01() {
+
+        //Set the url
+        specification.pathParams("first", "todos", "second", 198);
+
+        // Set the expected data / payload :
+        Map<String, Object> payload = new HashMap<>();
+        // Send the request and get the response :
+
+        Response response = given(specification).when().delete("{first}/{second}");
+
+        // Do Assertion
+
+        System.out.println("payload = " + payload);
+
+        // 1.Way
+        assertEquals(200, response.statusCode());
+
+        // 2.Way
+        response.then().statusCode(200);
+
+    }
+}
